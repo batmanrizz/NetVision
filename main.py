e2d4fc8a4063667b795b3b577b77d542abd351a1
Aryaman <aryamanrizz@gmail.com>
2025-02-14 22:46:09 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from app import socketio, app1
+from app import socketio, app
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000, debug=True, use_reloader=True, log_output=True)
